e20621582284d29e72611f84ec31e246b76109e5
Martin Frigaard <mjfrigaard@pm.me>
2023-07-10 16:15:46 -0700

    new shinytest2

--- tests/testthat/test-shinytest2-datasetApp.R
+++ tests/testthat/test-shinytest2-datasetApp.R
@@ -1,7 +1,7 @@
 library(shinytest2)
 
 test_that("{shinytest2}: datasetApp", {
-  ds_app <- msst2ap::datasetApp()
-  app <- AppDriver$new(ds_app)
+  app <- AppDriver$new(msst2ap::datasetApp(), height = 600, width = 800)
+  app$set_inputs(`dataset-dataset` = "attitude")
   app$expect_values()
 })
