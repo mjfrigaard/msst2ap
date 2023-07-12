b22b2303c57e9e76110b919adf162b874bab1af0
Martin Frigaard <mjfrigaard@pm.me>
2023-07-12 09:25:48 -0700

    add lobstr

--- R/msst2ap-package.R
+++ R/msst2ap-package.R
@@ -7,6 +7,7 @@
 ##
 #' @importFrom pkgload load_all
 #' @import rlang
+#' @import lobstr
 #' @import shinytest2
 #' @importFrom dplyr select where filter pull contains mutate glimpse arrange
 #' @importFrom rsconnect deployApp
